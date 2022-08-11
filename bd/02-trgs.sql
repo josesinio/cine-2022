1fb9e4dcd28a6a70d681820c209f64a9e0e18f65
quirogapabon11 <quirogapabon11@gmail.com>
2022-08-11 14:26:08 -0300

    Entrega-2 11/08/2022

--- bd/02-trgs.sql
+++ bd/02-trgs.sql
@@ -1,19 +1,22 @@
--- Realizar un trigger que verifique que al momento de insertar una entrada, no sobrepase la cantidad de entradas vendidas para la capacidad de la sala correspondiente, en ese caso no se debe permitir la operación y se tiene que mostrar la leyenda “Sala Llena”.
+-- 1 Realizar un trigger que verifique que al momento de insertar una entrada, no sobrepase la cantidad de entradas vendidas para la capacidad de la sala correspondiente a la proyección, en ese caso no se debe permitir la operación y se tiene que mostrar la leyenda “Sala Llena”.
 
-Delimiter $$
-drop trigger if exists 	befinsentrada $$
-create trigger befinsentrada before insert on entrada for each row
-begin
-			if (exists( select count(numEntrada)
-						from Entrada 
-                        join Proyeccion using (idProyeccion)
-                        join Sala S using (numSala)
-                        where S.numSala = new.numSala
-                        and count(numEntrada) > capacidad)) then 
-						signal sqlstate "45000"
-                        set message_text = "La sala esta llena";
-				end if;
-end $$ 
+Delimiter // 
+drop trigger if exists  BefInsEntrada //
+CREATE TRIGGER BefInsEntrada BEFORE INSERT ON Entrada For Each row 
+begin 
+	DECLARE cantidadEntradas TINYINT; 
+    Select COUNT(numEntrada) Into cantidadEntradas
+    from Entrada 
+    join Proyeccion Using (idProyeccion) 
+    WHERE idProyeccion = new.idProyeccion;
+    
+    IF (capacidad <= cantidadEntradas) then 
+    signal sqlstate "45000"
+	set message_text = "La sala esta llena";
+    end if;
+end //
+
+ 
 -- Realizar un trigger para que cada vez que se da de alta una película nueva, se crea una proyección por cada sala y para la fecha y hora de creación.
 DELIMITER $$
 DROP TRIGGER IF EXISTS aftInsPelicula $$
