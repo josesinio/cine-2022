1d4464f5b3c0ae83c5d6401e50c13e3146a03610
quirogapabon11 <quirogapabon11@gmail.com>
2022-09-14 18:00:23 -0300

    Update 03-user.sql

--- bd/03-user.sql
+++ bd/03-user.sql
@@ -11,3 +11,9 @@ grant select on CINE.* to  "Administrador"@"%";
 grant  insert, update on CINE.Proyeccion to  "Administrador"@"localhost";
 grant  insert, update on CINE.Pelicula to  "Administrador"@"localhost";
 grant select, insert, update on CINE.Sala to  "Administrador"@"localhost";
+
+-- Cajero: desde la red 10.3.45.xxx puede ver todas las tablas y puede insertar filas nuevas en Entrada.
+DROP USER IF EXISTS 'Cajero'@'10.3.45.%';
+CREATE USER IF EXISTS 'Cajero'@'10.3.45.%' IDENTIFIED BY 'cajeropass';
+GRANT SELECT ON CINE.* TO 'Cajero'@'10.3.45.%';
+GRANT INSERT ON CINE.Entrada TO 'Cajero'@'10.3.45.%';
