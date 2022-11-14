55554f0139fa22b2c37d76108076eba6ecd1ca9a
josesinio <jose.cruzet12d1@gmail.com>
2022-11-14 15:09:25 -0300

    mapEntrada
    falta corregir un error en la linea 56 .

--- src/Cine.AdoMySQL/MapEntrada.cs
+++ src/Cine.AdoMySQL/MapEntrada.cs
@@ -7,18 +7,80 @@ namespace Cine.AdoMySQL.Mapeadores
 {
     public class MapEntrada : Mapeador<Entrada>
     {
-        public MapProyeccion mapProyeccion { get; set; }
-        public MapSala mapSala { get; set; }
+        public MapProyeccion MapProyeccion { get; set; }
+        public MapSala MapSala { get; set; }
+        public MapCliente MapCliente { get; set; }
         public MapEntrada(AdoAGBD ado) : base(ado) => Tabla = "Entrada";
-        public MapEntrada(MapProyeccion mapProyeccion, MapSala mapSala) : this(mapProyeccion.AdoAGBD)
+        public MapEntrada(MapProyeccion mapProyeccion, MapSala mapSala, MapCliente mapCliente) : this(mapProyeccion.AdoAGBD)
         {
             MapProyeccion = mapProyeccion;
             MapSala = mapSala;
+            MapCliente = mapCliente;
         }
 
-        public override Entrada ObjetoDesdeFila(DataRow fila)
+        public override Entrada ObjetoDesdeFila(DataRow fila) => new Entrada
+        (
+            numEntrada: Convert.ToByte(fila["Id"]),
+            proyeccion: MapProyeccion.proyeccionesPorId(Convert.ToByte(fila["idProyeccion"])),
+            cliente: MapCliente.ClientePorId(Convert.ToByte(fila["IdCliente"])),
+            sala: MapSala.SalaPorId(Convert.ToByte(fila["IdSala"])),
+            capacidad: Convert.ToInt32(fila["capacidad"]),
+            valor: Convert.ToSingle(fila["valor"])
+        );
+
+        public void AltaEntrada(Entrada entrada)
         {
-
-
+            EjecutarComandoCon("altaEntrada", ConfigurarAltaEntrada, PostAltaEntrada, entrada);
         }
+
+
+        public void ConfigurarAltaEntrada(Entrada entrada)
+        {
+            SetComandoSP("altaEntrada");
+
+            BP.CrearParametroSalida("unIdEntrada")
+            .SetTipo(MySql.Data.MySqlClient.MySqlDbType.Byte)
+            .AgregarParametro();
+
+            BP.CrearParametroSalida("unidproyeccion")
+            .SetTipo(MySql.Data.MySqlClient.MySqlDbType.Byte)
+            .SetValor(entrada.Proyeccion.id)
+            .AgregarParametro();
+
+            BP.CrearParametroSalida("unIdCliente")
+            .SetTipo(MySql.Data.MySqlClient.MySqlDbType.Byte)
+            .SetValor(entrada.Cliente.id);
+
+            BP.CrearParametroSalida("unIdSala")
+            .SetTipo(MySql.Data.MySqlClient.MySqlDbType.Byte)
+            .SetValor(entrada.sala.numSala)
+            .AgregarParametro();
+
+            BP.CrearParametroSalida("Capacidad")
+            .SetTipo(MySql.Data.MySqlClient.MySqlDbType.Int32)
+            .AgregarParametro();
+
+            BP.CrearParametroSalida("valor")
+            .SetTipo(MySql.Data.MySqlClient.MySqlDbType.Float)
+            .AgregarParametro();
+        }
+
+        public void PostAltaEntrada(Entrada entrada)
+        {
+            var paramIdEntrada = GetParametro("unIdEntrada");
+            entrada.NumEntrada = Convert.ToByte(paramIdEntrada.Value);
+        }
+        public Entrada EntradaPorId(byte numEntrada)
+        {
+            SetComandoSP("EntradaPorId");
+
+            BP.CrearParametro("unIdEntrada")
+              .SetTipo(MySql.Data.MySqlClient.MySqlDbType.Byte)
+              .SetValor(numEntrada)
+              .AgregarParametro();
+
+            return ElementoDesdeSP();
+        }
+
     }
+}
--- src/Cine.Core/Entrada.cs
+++ src/Cine.Core/Entrada.cs
@@ -5,16 +5,16 @@ namespace Cine.Core
         public byte NumEntrada { get; set; }
         public Proyeccion Proyeccion { get; set; }
         public Cliente Cliente { get; set; }
-        public int NumSala { get; set; }
+        public Sala NumSala { get; set; }
         public int Capacidad { get; set; }
         public float Valor { get; set; }
 
-        public Entrada(byte numEntrada, Proyeccion proyeccion, Cliente cliente, int numSala, int capacidad, float valor)
+        public Entrada(byte numEntrada, Proyeccion proyeccion, Cliente cliente, Sala sala, int capacidad, float valor)
         {
             this.NumEntrada = numEntrada;
             this.Proyeccion = proyeccion;
             this.Cliente = cliente;
-            this.NumSala = numSala;
+            this.NumSala = sala;
             this.Capacidad = capacidad;
             this.Valor = valor;
         }
