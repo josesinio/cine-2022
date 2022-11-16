1779c313d6b7dbd3d9e9ddc0629b5766ee982e2e
b <duranpbrenda@gmail.com>
2022-11-16 17:08:50 -0300

    Update PeliculaTest.cs
    testpelicula

--- src/Cine.AdoMySQL.Test/PeliculaTest.cs
+++ src/Cine.AdoMySQL.Test/PeliculaTest.cs
@@ -1,9 +1,21 @@
+using Cine.AdoMySQL.Mapeadores;
+using Cine.Core;
+using et12.edu.ar.AGBD.Ado;
+
 namespace Cine.AdoMySQL.Test;
 public class PeliculaTest
 {
-    [Fact]
-    public void Test1()
+    public AdoCine AdoCine{get; set;}
+    public PeliculaTest()
     {
-        Assert.True(true);
+        var adoAGBD = FactoryAdoAGBD.GetAdoMySQL("appeSetting.json", "test");
+        AdoCine = new AdoCine(adoAGBD);
+    }
+    [Fact]
+    public void altaPelicula()
+    {
+        var pelicula = new Pelicula(1,"son como niños 2", 2004-03-01 18:30, 1);
+        AdoCine.altaPelicula(pelicula);
+        Assert.Equal(1, nombre, estreno, genero.Id);
     }
 }
